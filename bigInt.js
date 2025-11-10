768755b0e4fa901825f8c4f74563cee1156c3d4a
Mohammedhasmi-Patel <patelmohammedhasmi@gmail.com>
2025-11-10 23:47:12 +0530

    learn about bigint

---
+++ bigInt.js
@@ -0,0 +1,4 @@
+console.log("Bigint example is started.... ");
+var bigValue = 120000000n;
+var x = 12n;
+var sum = bigValue + x;
---
+++ bigInt.ts
@@ -0,0 +1,8 @@
+console.log("Bigint example is started.... ");
+
+var bigValue : bigint = 120000000n;
+var x : bigint = 12n;
+
+var sum : bigint = bigValue + x;
+
+
