d7634f604d96d9e8b70dd57c1bfac0e2db062efc
Mohammedhasmi-Patel <patelmohammedhasmi@gmail.com>
2025-11-10 23:56:45 +0530

    learn about inputs in html & ts

---
+++ inputs/index.js
@@ -0,0 +1,8 @@
+function getInfo() {
+    var nameInput = document.getElementById("username");
+    console.log(nameInput.value);
+    var emailValue = document.getElementById("email");
+    console.log(emailValue.value);
+    var age = document.getElementById("age");
+    console.log(age.value);
+}
---
+++ inputs/index.ts
@@ -0,0 +1,14 @@
+function getInfo(){
+    const nameInput = document.getElementById("username") as HTMLInputElement;
+    console.log(nameInput.value);
+
+    const emailValue = document.getElementById("email") as HTMLInputElement;
+    console.log(emailValue.value);
+
+    const age = document.getElementById("age") as HTMLInputElement;
+    console.log(age.value);
+
+
+
+
+}
